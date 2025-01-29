b9d4b4dee698a46ab07bdefaf0a0296fb503ff9e
Garrett <garrett.weber@galaxy.com>
2025-01-29 10:33:55 -0700

    Update integer_overflow_1.sol

--- integer_overflow/integer_overflow_1.sol
+++ integer_overflow/integer_overflow_1.sol
@@ -1,17 +1,9 @@
 pragma solidity ^0.4.15;
 
 contract Overflow {
-    uint private sellerBalance=0;
+    uint private debt = 0;
     
-    function add(uint value) returns (bool){
-        sellerBalance += value; // possible overflow
-
-        // possible auditor assert
-        // assert(sellerBalance >= value); 
-    } 
-
-    function safe_add(uint value) returns (bool){
-        require(value + sellerBalance >= sellerBalance);
-        sellerBalance += value; 
+    function increase_debt(uint debt_inc) {
+        debt += debt_inc;
     } 
 }
